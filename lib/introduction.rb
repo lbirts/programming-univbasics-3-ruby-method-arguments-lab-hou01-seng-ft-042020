f9352efd6edc3d6d1cb78d94093acd6e502cf51e
Lauren Birts <birtslauren@gmail.com>
2020-03-16 12:38:57 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -14,6 +14,9 @@ def introduction_with_language_optional(name, language = "Ruby")
   "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
+introduction("Josh")
+introduction("Dan")
+
 
 
 
